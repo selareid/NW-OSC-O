5a05c66ead7ed21416c64a720453ca08d19a944b
Daniel Reid <danielselareid40@gmail.com>
2016-09-09 17:25:29 +1200

    Beginner Script
    The beginner script for when you start.

---
+++ role.wallRepairer.js
@@ -0,0 +1,35 @@
+var roleRepairer = require ('role.repairer');
+
+module.exports = {
+    run: function(creep) {
+        if (creep.memory.working == true && creep.carry.energy == 0) {
+            creep.memory.working = false;
+        }
+        else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.working = true;
+        }
+
+        if (creep.memory.working == true) {
+            var percentOfWall;
+            var structure = creep.pos.findClosestByPath(FIND_STRUCTURES,
+                {filter: (s) => s.structureType == STRUCTURE_WALL && s.hits <= s.hitsMax*percentOfWall});
+
+            if (structure != undefined) {
+                if (creep.repair(structure) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(structure);
+                }
+            }
+            else {
+                roleRepairer.run(creep);
+            }
+
+        }
+        else {
+            var source = creep.pos.findClosestByPath(FIND_SOURCES);
+            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(source);
+            }
+
+        }
+    }
+};
