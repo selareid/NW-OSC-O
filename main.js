0aecd2f1407ef78bb7bb9692bb1d69da98d6fe2a
Daniel Reid <danielselareid40@gmail.com>
2016-09-11 17:15:34 +1200

    made it so it doesn’t do stuff if spawn is not defined

--- main.js
+++ main.js
@@ -28,61 +28,65 @@ module.exports.loop = function () {
         var spawn = room.find(FIND_MY_SPAWNS)[0];
         var mainSpawn = Game.spawns.Spawn1;
 
-        for (let name in Game.creeps) {
-            var creep = Game.creeps[name];
+        if (spawn != undefined) {
 
-            if (creep.memory.role == 'harvester') {
-                roleHarvester.run(creep, spawn);
-            }
-            else if (creep.memory.role == 'distributor') {
-                roleDistributor.run(creep, spawn);
-            }
-            else if (creep.memory.role == 'energyMover') {
-                roleEnergyMover.run(creep, spawn);
-            }
-            else if (creep.memory.role == 'upgrader') {
-                roleUpgrader.run(creep, spawn);
-            }
-            else if (creep.memory.role == 'builder') {
-                roleBuilder.run(creep, spawn);
-            }
-            else if (creep.memory.role == 'repairer') {
-                roleRepairer.run(creep, spawn);
-            }
-            else if (creep.memory.role == 'wallRepairer') {
-                roleWallRepairer.run(creep, spawn);
-            }
-            else if (creep.memory.role == 'claimer') {
+            for (let name in Game.creeps) {
+                var creep = Game.creeps[name];
 
-                var roomToGoTo = 'E53N13'
-                var roomToClaim = 'E52N13';
-                roleClaimer.run(creep, mainSpawn, roomToGoTo, roomToClaim);
+                if (creep.memory.role == 'harvester') {
+                    roleHarvester.run(creep, spawn);
+                }
+                else if (creep.memory.role == 'distributor') {
+                    roleDistributor.run(creep, spawn);
+                }
+                else if (creep.memory.role == 'energyMover') {
+                    roleEnergyMover.run(creep, spawn);
+                }
+                else if (creep.memory.role == 'upgrader') {
+                    roleUpgrader.run(creep, spawn);
+                }
+                else if (creep.memory.role == 'builder') {
+                    roleBuilder.run(creep, spawn);
+                }
+                else if (creep.memory.role == 'repairer') {
+                    roleRepairer.run(creep, spawn);
+                }
+                else if (creep.memory.role == 'wallRepairer') {
+                    roleWallRepairer.run(creep, spawn);
+                }
+                else if (creep.memory.role == 'claimer') {
 
-            }
-            else if (creep.memory.role == 'otherRoomCreep') {
-                var roomInPath = 'E53N13';
-                var roomToGoTo = 'E52N13';
-                roleOtherRoomCreep.run(creep, mainSpawn, roomInPath, roomToGoTo);
-            }
-            else if (creep.memory.role == 'energyThief') {
-                var roomToGoTo = 'E53N13';
-                var roomToTakeFrom = 'E52N13';
-                roleEnergyThief.run(creep, mainSpawn, roomToGoTo, roomToTakeFrom);
-            }
-            else if (creep.memory.role == 'invader') {
-                roleInvader.run(creep, mainSpawn);
-            }
-            else {
-                console.log('ERR UNKNOWN CREEP TYPE ' + creep.memory.role + ' Creep name = ' + creep.name);
-            }
+                    var roomToGoTo = 'E53N13'
+                    var roomToClaim = 'E52N13';
+                    roleClaimer.run(creep, mainSpawn, roomToGoTo, roomToClaim);
 
-            AISpawner.loop(spawn, mainSpawn);
+                }
+                else if (creep.memory.role == 'otherRoomCreep') {
+                    var roomInPath = 'E53N13';
+                    var roomToGoTo = 'E52N13';
+                    roleOtherRoomCreep.run(creep, mainSpawn, roomInPath, roomToGoTo);
+                }
+                else if (creep.memory.role == 'energyThief') {
+                    var roomToGoTo = 'E53N13';
+                    var roomToTakeFrom = 'E52N13';
+                    roleEnergyThief.run(creep, mainSpawn, roomToGoTo, roomToTakeFrom);
+                }
+                else if (creep.memory.role == 'invader') {
+                    roleInvader.run(creep, mainSpawn);
+                }
+                else {
+                    console.log('ERR UNKNOWN CREEP TYPE ' + creep.memory.role + ' Creep name = ' + creep.name);
+                }
 
-            var towers = room.find(FIND_STRUCTURES, {
-                filter: (s) => s.structureType == STRUCTURE_TOWER
-            });
-            for (let tower of towers) {
-                runTower.run(tower);
+
+                AISpawner.loop(spawn, mainSpawn);
+
+                var towers = room.find(FIND_STRUCTURES, {
+                    filter: (s) => s.structureType == STRUCTURE_TOWER
+                });
+                for (let tower of towers) {
+                    runTower.run(tower);
+                }
             }
         }
     }
